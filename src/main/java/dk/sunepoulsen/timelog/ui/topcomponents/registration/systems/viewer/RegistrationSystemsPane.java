4dca248416d7cc300118d4dd83fe00ee55b2f9f1
sunepoulsen <sunepoulsen@gmail.com>
2017-09-24 17:55:50 +0200

    Simpler ui for registration systems.

--- src/main/java/dk/sunepoulsen/timelog/ui/topcomponents/registration/systems/viewer/RegistrationSystemsPane.java
+++ src/main/java/dk/sunepoulsen/timelog/ui/topcomponents/registration/systems/viewer/RegistrationSystemsPane.java
@@ -2,13 +2,13 @@ package dk.sunepoulsen.timelog.ui.topcomponents.registration.systems.viewer;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import lombok.extern.slf4j.XSlf4j;
 
 import java.io.IOException;
 
 @XSlf4j
-public class RegistrationSystemsPane extends BorderPane {
+public class RegistrationSystemsPane extends AnchorPane {
     public RegistrationSystemsPane() {
         FXMLLoader fxmlLoader = new FXMLLoader( getClass().getResource( "viewer.fxml" ) );
         fxmlLoader.setRoot( this );
